830e791f6895ef718986f4118065a4aa40e4f111
mehmetpektass <pektasmehmett1184@gmail.com>
2024-04-25 21:11:55 +0300

    edited navbar.jsx and navbar.css

--- project/client/src/App.jsx
+++ project/client/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar/navbar'
 const App = () => {
   return (
     <>
-    <div>
+    <div className='app'>
       <Navbar/>
     </div>
     </>
--- project/client/src/components/Navbar/navbar.jsx
+++ project/client/src/components/Navbar/navbar.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import { adminAssets } from "../../assets/admin_assets/assets.js";
 import { frontendAssets } from "../../assets/frontend_assets/assets";
 
 const Navbar = () => {
+    const [menu , setMenu] = useState('home');
   return (
     <div className="navbar">
       <img src={adminAssets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li>HOME</li>
-        <li>MENU</li>
-        <li>MOBILE-APP</li>
-        <li>CONTACT US</li>
+        <li onClick={() => setMenu('home')} className={menu=== 'home' ? 'active' : ''}>HOME</li>
+        <li onClick={() => setMenu('menu')} className={menu=== 'menu' ? 'active' : ''}>MENU</li>
+        <li onClick={() => setMenu('mobileApp')} className={menu=== 'mobileApp' ? 'active' : ''}>MOBILE-APP</li>
+        <li onClick={() => setMenu('contactUs')} className={menu=== 'contactUs' ? 'active' : ''}>CONTACT US</li>
       </ul>
       <div className="navbar-right">
         <img src={frontendAssets.search_icon} alt="" />
