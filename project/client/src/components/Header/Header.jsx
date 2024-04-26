b56d6e635d572a3e8b1bcc20bf6decf5a1bbe076
mehmetpektass <pektasmehmett1184@gmail.com>
2024-04-26 10:50:20 +0300

    edited header

--- project/client/src/components/Header/Header.jsx
+++ project/client/src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="header">
+      <div className="header-contents">
+        <h2>Choose Your Favourite Food Here</h2>
+        <p>
+          "Craving something delicious? You're in the right place!
+          <span> Fancy</span> brings you a world of flavors right to your
+          doorstep. Browse our diverse restaurants, find your favorites or try
+          something new.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
