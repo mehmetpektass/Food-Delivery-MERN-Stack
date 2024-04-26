3c286dddb383c7d6f7231aee78cc6038829b071b
mehmetpektass <pektasmehmett1184@gmail.com>
2024-04-26 23:50:39 +0300

    edited food items counter

--- project/client/src/components/FoodItem/FoodItem.jsx
+++ project/client/src/components/FoodItem/FoodItem.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import './FoodItem.css'
+import React, { useState } from "react";
+import "./FoodItem.css";
+import { frontendAssets } from "../../assets/frontend_assets/assets.js";
 
-const FoodItem = ({id , name, price, description, image}) => {
+const FoodItem = ({ id, name, price, description, image }) => {
+  const [itemCount, setItemCount] = useState(0);
+
   return (
-    <div className='food-item'>
-        <div className='food-iitem-img-container'>
-            <img className='food-item-image' src={image} alt="" />
+    <div className="food-item">
+      <div className="food-iitem-img-container">
+        <img className="food-item-image" src={image} alt="" />
+        {!itemCount ? (
+          <img
+            className="add"
+            onClick={() => setItemCount((prev) => prev + 1)}
+            src={frontendAssets.add_icon_white}
+          ></img>
+        ) : (
+          <div className="food-item-counter">
+            <img onClick={() => setItemCount(prev=>prev-1)} src={frontendAssets.remove_icon_red} alt="" />
+            <p>{itemCount}</p>
+            <img onClick={() => setItemCount(prev=>prev+1)} src={frontendAssets.add_icon_green} alt="" />
+          </div>
+        )}
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name">
+          <p>{name}</p>
         </div>
-        <div className='food-item-info'>
-            <div className='food-item-name'>
-                <p>{name}</p>
-            </div>
-            <p className='food-item-description'>{description}</p>
-            <p className='food-item-price'>${price}</p>
-        </div>
-
+        <p className="food-item-description">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodItem
+export default FoodItem;
