fcef2ae0159379ec7ad4242e32380fad2ae5b1c3
mehmetpektass <pektasmehmett1184@gmail.com>
2024-04-27 22:53:36 +0300

    edited text of explore menu

--- project/client/src/components/ExploreMenu/ExploreMenu.jsx
+++ project/client/src/components/ExploreMenu/ExploreMenu.jsx
@@ -6,7 +6,7 @@ const ExploreMenu = ({category , setCategory}) => {
   return (
    <div className='explore-menu' id='explore-menu'>
         <h1>Explore Our Menu</h1>
-        <p className='explore-menu-text'>Choose From A Diverse Menu You Want To Eat</p>
+        <p className='explore-menu-text'></p>
         <div className='explore-menu-list'>
             {menu_list.map ((item , index) => {
                 return (
