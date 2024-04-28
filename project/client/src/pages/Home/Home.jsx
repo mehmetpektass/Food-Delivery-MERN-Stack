579d45e4d148c2a74cfc4a34d03e23cc975346a4
mehmetpektass <pektasmehmett1184@gmail.com>
2024-04-28 23:50:33 +0300

    edited content of home page back

--- project/client/src/pages/Home/Home.jsx
+++ project/client/src/pages/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
-      
+      <FoodDisplay category={category}/>
     </>
   );
 };
