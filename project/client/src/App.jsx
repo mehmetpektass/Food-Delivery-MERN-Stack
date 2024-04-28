4e5ec0f4d8cfafcab5efced1b6c8bef2bec29e74
mehmetpektass <pektasmehmett1184@gmail.com>
2024-04-28 23:49:32 +0300

    edited route on app.jsx

--- project/client/src/App.jsx
+++ project/client/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>} />
-        <Route path='/order' element={<PlaceOrder/>} />
+        
       </Routes>
     </div>
     </>
