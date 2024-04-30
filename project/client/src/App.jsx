efc4dbf974b75900d066a3505c0ac37088aba7d5
mehmetpektass <pektasmehmett1184@gmail.com>
2024-04-30 13:56:40 +0300

    created log in page text

--- project/client/src/App.jsx
+++ project/client/src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Cart from './pages/Cart/Cart.jsx'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
 import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 
 const App = () => {
+  const [showLogin , setShowLogin] = useState(false);
   return (
     <>
+    {showLogin?(<LoginPopup setShowLogin={setShowLogin}/>):(<></>)}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>} />
--- project/client/src/components/LoginPopup/LoginPopup.jsx
+++ project/client/src/components/LoginPopup/LoginPopup.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginPopup.css'
+import { frontendAssets } from '../../assets/frontend_assets/assets'
 
-const LoginPopup = () => {
+const LoginPopup = ({setShowLogin}) => {
+    const [currentState, setCurrentState] = useState('Log In')
   return (
-    <div>LoginPopup</div>
+   <div className='login-popup'>
+    <form className='login-popup-container'>
+        <div className='login-popup-title'>
+            <h2>{currentState}</h2>
+            <img onClick={() => setShowLogin(false)} src={frontendAssets.cross_icon} alt="" />
+        </div>
+        <div className='login-popup-inputs'>
+            {currentState==='Log In'? (<></>):(<input type="text" placeholder='Your Name' required/>)}
+            <input type="email" placeholder='Your Email' required/>
+            <input type="password" placeholder='Your Password' required/>
+        </div>
+        <button>{currentState==='Sign Up'? ('Create Account'):('Log In')}</button>
+        <div className='login-popup-condition'>
+            <input type="checkbox" required/>
+            <p>By Continuing, I Agree to The Terms of Use & Privacy Policy</p>
+        </div>
+        {currentState==='Log In'? ( <p>Create An Account? <span onClick={() => setCurrentState('Sign Up')} >Click Here</span></p>):(<p>Already Have An Account? <span onClick={() => setCurrentState('Log In')}>Login Here</span></p>)}
+       
+        
+    </form>
+   </div>
   )
 }
 
