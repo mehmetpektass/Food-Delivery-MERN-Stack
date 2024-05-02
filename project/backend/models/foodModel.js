8c6067c1b0d1486e82ccce783d0ee1cbba017885
mehmetpektass <pektasmehmett1184@gmail.com>
2024-05-02 23:17:59 +0300

    edited food model

--- project/backend/models/foodModel.js
+++ project/backend/models/foodModel.js
@@ -1,9 +1,26 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
 const foodSchema = new mongoose.Schema({
-    name: {type:String , required:true},
-    description: {type:String , required:true},
-    price: {type:Number , required:true},
-    image: {type:String , required:true},
-    category: {type:String , required:true}
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+});
+
+export const foodModel = mongoose.model.food || mongoose.model('food' , foodSchema)
