a5aa36b19545f7d6524023137212ee37652ba543
mehmetpektass <pektasmehmett1184@gmail.com>
2024-05-03 00:20:14 +0300

    edited multer structure

--- project/backend/routes/foodRoute.js
+++ project/backend/routes/foodRoute.js
@@ -4,8 +4,15 @@ import multer from 'multer';
 
 const foodRouter = express.Router();
 
+const storage = multer.diskStorage({
+    destination:'uploads',
+    filename:(req,file,cb) =>{
+        return cb(null , `${Date.now()}${file.originalname}`)
+    }
+})
 
+const upload = multer({storage:storage})
 
-foodRouter.post('/add' , addFood)
+foodRouter.post('/add' ,upload.single('image'), addFood)
 
 export default foodRouter;
