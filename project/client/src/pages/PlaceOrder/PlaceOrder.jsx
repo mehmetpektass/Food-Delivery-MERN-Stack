eeec72e73c8f0ee8585338e0730ec0bad0cb4b0b
mehmetpektass <pektasmehmett1184@gmail.com>
2024-05-02 08:20:45 +0300

    created css file for place order page

--- project/client/src/pages/PlaceOrder/PlaceOrder.jsx
+++ project/client/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -7,6 +7,7 @@ const PlaceOrder = () => {
   return (
     <form className="place-order">
       <div className="place-order-left">
+        <p className="title">Delivery Information</p>
         <div className="multi-fields">
           <input type="text" placeholder="First Name" />
           <input type="text" placeholder="Last Name" />
