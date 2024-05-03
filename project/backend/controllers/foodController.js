cfc7d5a646b17116df2579d0d60bb817e1d06e9b
mehmetpektass <pektasmehmett1184@gmail.com>
2024-05-03 22:20:12 +0300

    edited text

--- project/backend/controllers/foodController.js
+++ project/backend/controllers/foodController.js
@@ -11,7 +11,7 @@ const addFood = async (req, res) => {
   });
   try {
     await food.save();
-    res.json({ success: true, message: "Food Added" });
+    res.json({ success: true, message: "" });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: "Error" });
