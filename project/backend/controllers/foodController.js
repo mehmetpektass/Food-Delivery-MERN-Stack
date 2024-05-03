3f92d961eae2c3266643e0df5a1a021c3f76028d
mehmetpektass <pektasmehmett1184@gmail.com>
2024-05-03 22:20:57 +0300

    edited error  text back

--- project/backend/controllers/foodController.js
+++ project/backend/controllers/foodController.js
@@ -14,7 +14,7 @@ const addFood = async (req, res) => {
     res.json({ success: true, message: "Food Added" });
   } catch (error) {
     console.log(error);
-    res.json({ success: false, message: "Error" });
+    res.json({ success: false, message: "" });
   }
 };
 
