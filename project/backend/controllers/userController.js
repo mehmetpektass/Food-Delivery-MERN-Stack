bf551b3b172156457de01cf159928fefec765845
mehmetpektass <pektasmehmett1184@gmail.com>
2024-05-14 12:50:15 +0300

    created routes on routes and server files

---
+++ project/backend/controllers/userController.js
@@ -0,0 +1,13 @@
+import userModel from "../models/userModel.js";
+import  Jwt  from "jsonwebtoken";
+import bcrypt from 'bcrypt'
+import validator from "validator";
+
+const registerUser = async (req,res) => {
+
+}
+
+const loginUser = async (req,res) => {
+
+}
+export {registerUser , loginUser}
