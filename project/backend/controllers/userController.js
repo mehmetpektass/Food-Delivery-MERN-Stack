b46ee928ec89f903626e9f4f89aa7f9a101a8b85
mehmetpektass <pektasmehmett1184@gmail.com>
2024-05-14 15:20:55 +0300

    created register backend code

--- project/backend/controllers/userController.js
+++ project/backend/controllers/userController.js
@@ -1,13 +1,46 @@
 import userModel from "../models/userModel.js";
-import  Jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import bcrypt from 'bcrypt'
 import validator from "validator";
 
-const registerUser = async (req,res) => {
-
+const createToken = (id) => {
+    jwt.sign({id} , process.env.JWT_SECRET)
 }
 
 const loginUser = async (req,res) => {
+  
+}
 
+const registerUser = async (req,res) => {
+    const {name , password , email} = req.body;
+    try {
+        const exists = await userModel.findOne({email})
+        if (exists) {
+            return res.json({success:false, message:'User Already Exists'})
+        }
+        if (!validator.isEmail(email)) {
+            return res.json({success:false, message:'Please Enter a Valid Email'})
+        }
+        if (password.length < 8) {
+            return res.json({success:false, message:'Please Enter a Strong Password'})
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword,
+        })
+
+        const user = await newUser.save();
+        const token = createToken(user._id)
+        res.json({success:true,token})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:'Error'})
+    }
 }
 export {registerUser , loginUser}
--- project/backend/server.js
+++ project/backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
+import 'dotenv/config.js'
 
 const app = express();
 const port = 4000;
@@ -14,7 +15,7 @@ connectDB();
 
 app.use('/api/food' ,foodRouter)
 app.use('/images', express.static('uploads'))
-app.use('api/user', userRouter)
+app.use('/api/user', userRouter)
 
 app.get("/", (req, res) => {
   res.json("API Working");
