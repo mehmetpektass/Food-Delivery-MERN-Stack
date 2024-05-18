a8740d258ea485fe47be8211d5c7fe9c5cfc8acf
mehmetpektass <pektasmehmett1184@gmail.com>
2024-05-18 17:16:26 +0300

    edited verify token for users

--- project/backend/controllers/cartController.js
+++ project/backend/controllers/cartController.js
@@ -1,7 +1,21 @@
 import userModel from "../models/userModel";
 
 const addToCart = async (req,res) => {
-
+    try {
+        let userData = await userModel.findOne({_id:req.body.id});
+        const cartData = await userData.cartData;
+        if (!cartData[req.body.itemId]) {
+            cartData[req.body.itemId] = 1
+        }
+        else{
+            cartData[req.body.itemId] = +1
+        }
+        await userModel.findByIdAndUpdate(req.body.id,{cartData })
+        res.json({success:true , message:'Added to Cart'})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false , message:'Error'})
+    }
 }
 
 const removeFromCart = async (req,res) => {
