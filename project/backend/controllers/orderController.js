afd9da4d91fde5164b522c49be7c1fe300aad300
mehmetpektass <pektasmehmett1184@gmail.com>
2024-05-19 13:38:09 +0300

    edited order functions

---
+++ project/backend/controllers/orderController.js
@@ -0,0 +1,9 @@
+import userModel from "../models/userModel.js";
+import orderModel from "../models/orderModel.js";
+import Stripe from "stripe";
+
+const placeOrder = async (req , res) => {
+
+}
+
+export {placeOrder}
